f06105a58a4301194c64768f78ddfe1b7b41b6a2
Troy McCall <troyxmccall@gmail.com>
2018-09-18 19:51:22 -0400

    fix tr on osx

--- example/scripts/random-worker.sh
+++ example/scripts/random-worker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C
 count=$1
 for i in $(seq 1 $count) ;do
-    sleep .$RANDOM
-    echo "$2 worker ($i/$1): $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $((RANDOM%80 + 1)) | head -n 1)"
+  sleep .$RANDOM
+  echo "$2 worker ($i/$1): $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $((RANDOM%80 + 1)) | head -n 1)"
 done
